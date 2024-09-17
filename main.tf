deb45c8efa6d906a904e233c824553593e930e83
bhabatosh11 <pagalbhabatosh@gmail.com>
2024-09-17 14:11:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,10 @@
 provider "google" {
   # Configuration options
 }
-resource "google_storage_bucket" "gcs_bucket" {
-  name          = "testjenkins"
-  location      = "asia-south1"
-  force_destroy = true
-public_access_prevention = "enforced"
+resource "google_storage_bucket" "my-bucket" {
+  name                     = "tt-githubdemo-bucket-001"
+  project                  = "smart-portfolio-434304-g6"
+  location                 = "ASIA"
+  force_destroy            = true
+  public_access_prevention = "enforced"
 }
